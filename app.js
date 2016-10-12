15cf53f303986f96192c4f672df5d9fac24a0071
Jerome1337 <p-jerome@hotmail.fr>
2016-10-12 21:45:54 +0200

    Update JS to ES6

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 
-var app = express();
+let routes = require('./routes/index');
+let users = require('./routes/users');
+
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +34,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let express = require('express');
+let router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'EPSI NORD' });
+});
+
+router.post('/', function(req, res, next) {
+  let username = req.body,
+      password = req.body,
+      promo = req.body;
+  let town = promo.split(' '),
+      year = promo.split(' ');
+
+  let data = {
+    'form.submitted': 1,
+    came_from: 'http://ecampusnord.epsi.fr/',
+    __ac_name: username,
+    __ac_password: password,
+  };
+
+  login(data)
+      .then(function (account) {
+        res
+            .cookie('account', eval(account.value), { httpOnly: true })
+            .redirect(`/api/${town}/${year}/calendar/load`);
+      })
+      .catch(function (error) {
+        next(error);
+      });
 });
 
 module.exports = router;
