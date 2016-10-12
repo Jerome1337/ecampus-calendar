9b57d0d864b5f24a5dc87d296f8251acc5716acf
Jerome1337 <p-jerome@hotmail.fr>
2016-10-12 21:42:01 +0200

    Update index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let path = require('path');
+let router = express.Router();
+let settings = require(path.join(__dirname, '..', 'settings', 'settings.json'));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { settings });
 });
 
 module.exports = router;
