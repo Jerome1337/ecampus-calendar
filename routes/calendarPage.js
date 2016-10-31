42e702f286ecf6e733350cf13be1f7f39f4dcdfb
Bouquet <julien.bouquet@epsi.fr>
2016-10-31 12:33:58 +0100

    Migration MySQL to MongoDB

--- routes/calendarPage.js
+++ routes/calendarPage.js
@@ -1,35 +1,16 @@
 'use strict';
 
+let authHelper = require('./../authHelper');
 let express = require('express');
-let mysql = require('mysql');
-let path = require('path');
 let router = express.Router();
 let session = require('express-session');
-let settings = require(path.join(__dirname, '..', 'settings', 'settings.json'));
-let authHelper = require('./../authHelper');
 
 router.get('/:city/:promo/:status/:spe/calendar/:startDate/:endDate', function (req, res) {
-    let {city, promo, status, spe, startDate, endDate} = req.params;
-
-    let con = mysql.createConnection({
-        host: settings.db.host,
-        user: settings.db.user,
-        password: settings.db.password,
-        database: settings.db.database
-    });
-
-    let calendar_id = '';
-
     let authUrl = authHelper.getAuthUrl();
 
-    con.query('SELECT id FROM calendar WHERE (city = \'' + city + '\' AND promo = \'' + promo + '\' AND status = ' + status + ' AND specialite = \'' + spe + '\')', function (err, result) {
-        con.query('SELECT * FROM course WHERE (calendar_id = ' + result[0].id + ' AND date BETWEEN \'' + startDate + '\' AND \'' + endDate + '\') ORDER BY date', function (err, results) {
-            res.render('calendar', {
-                title: 'EPSI Ecampus calendar',
-                courses: results,
-                authUrl: authUrl
-            });
-        });
+    res.render('calendar', {
+        title: 'EPSI Ecampus calendar',
+        authUrl: authUrl
     });
 });
 
